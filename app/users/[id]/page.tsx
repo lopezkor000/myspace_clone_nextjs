0d2673670893e21b27dc4f3f1bf218ed5d0592ea
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-01-19 21:26:00 -0600

    following users

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import FollowButton from "@/components/FollowButton/FollowButton";
 import { prisma } from "@/lib/prisma";
 import { Metadata } from "next";
 
@@ -24,6 +25,8 @@ export default async function UserProfile(props: Props) {
       <img width={300} src={image ?? "next.svg"} alt={`${name}'s profile`} />
       <h3>Bio</h3>
       <p>{bio}</p>
+
+      <FollowButton targetUserId={params.id} />
     </div>
   );
 }
